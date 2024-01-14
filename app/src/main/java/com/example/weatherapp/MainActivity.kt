fb7d597d31bff33de702bac8e2ef0f7f8b93cfb6
Mateusz Wojteczek <56107289+wojmat@users.noreply.github.com>
2024-01-14 18:58:08 +0100

    Adding layout.
    Implementation of activity_main.xml to work with fully operational app.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -15,14 +15,14 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
 
     // These are private constants for the city and API key used for weather data requests. This is likely to be changed as we consider using an edit text in layout and search for a city.
-    private val city = "wroclaw,pl"
+    private val city = "london,gb"
     private val api = "69099485ea4553bae8fc0e52841ce693"
 
     // This is the onCreate method, which is called when the activity is created.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // This line would set the content view to the layout defined in activity_main.xml (currently commented out). Now we focus on testing the API calls.
-        // setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         fetchWeatherData(city)
     }
 
@@ -64,47 +64,47 @@ class MainActivity : AppCompatActivity() {
     private suspend fun updateUI(result: String) = withContext(Dispatchers.Main) {
         try {
             // The commented out code here would parse the JSON result and update UI elements accordingly.
-//                val jsonObj = JSONObject(result)
-//                val main = jsonObj.getJSONObject("main")
-//                val sys = jsonObj.getJSONObject("sys")
-//                val wind = jsonObj.getJSONObject("wind")
-//                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-//
-//                val updatedAt:Long = jsonObj.getLong("dt")
-//                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
-//                val temp = main.getString("temp")+"°C"
-//                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
-//                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
-//                val pressure = main.getString("pressure")
-//                val humidity = main.getString("humidity")
-//
-//                val sunrise:Long = sys.getLong("sunrise")
-//                val sunset:Long = sys.getLong("sunset")
-//                val windSpeed = wind.getString("speed")
-//                val weatherDescription = weather.getString("description")
-//
-//                val address = jsonObj.getString("name")+", "+sys.getString("country")
-//
-//                /* Populating extracted data into our views */
-//                findViewById<TextView>(R.id.address).text = address
-//                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
-//                findViewById<TextView>(R.id.status).text = weatherDescription.replaceFirstChar {
-//                    if (it.isLowerCase()) it.titlecase(
-//                        Locale.getDefault()
-//                    ) else it.toString()
-//                }
-//                findViewById<TextView>(R.id.temp).text = temp
-//                findViewById<TextView>(R.id.temp_min).text = tempMin
-//                findViewById<TextView>(R.id.temp_max).text = tempMax
-//                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-//                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-//                findViewById<TextView>(R.id.wind).text = windSpeed
-//                findViewById<TextView>(R.id.pressure).text = pressure
-//                findViewById<TextView>(R.id.humidity).text = humidity
-//
-//                /* Views populated, Hiding the loader, Showing the main design */
-//            findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp")+"°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+
+                val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+                /* Populating extracted data into our views */
+                findViewById<TextView>(R.id.address).text = address
+                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
+                findViewById<TextView>(R.id.status).text = weatherDescription.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(
+                        Locale.getDefault()
+                    ) else it.toString()
+                }
+                findViewById<TextView>(R.id.temp).text = temp
+                findViewById<TextView>(R.id.temp_min).text = tempMin
+                findViewById<TextView>(R.id.temp_max).text = tempMax
+                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.wind).text = windSpeed
+                findViewById<TextView>(R.id.pressure).text = pressure
+                findViewById<TextView>(R.id.humidity).text = humidity
+
+                /* Views populated, Hiding the loader, Showing the main design */
+            findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
 
             // Logging the received data for debugging.
             Log.d("API_RESULT", "Received data: $result")
@@ -114,8 +114,8 @@ class MainActivity : AppCompatActivity() {
             // Logging an error if there is an issue processing the result.
             Log.e("API_ERROR", "Error processing result: ${e.message}")
 
-//            findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//            findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+            findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
         }
     }
 }
